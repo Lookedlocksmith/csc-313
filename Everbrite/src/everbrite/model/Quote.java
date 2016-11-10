ded6ddc146db64248b997da65e780a1962e45787
Whitehurst <Whitehurst@I3-623-ITB>
2016-11-10 12:06:42 -0500

    Created Quote class

---
+++ Everbrite/src/everbrite/model/Quote.java
@@ -0,0 +1,28 @@
+
+package everbrite.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * A quote contains a list of materials and a list of processes
+ * as well as summary information.
+ * 
+ * @author Whitehurst
+ */
+public class Quote {
+    
+    private int projectNum;
+    private double overHeadRate;
+    private String customer;
+    private String comment;
+    private boolean complete;
+    private Date completionDate;
+    private ArrayList materials;
+    private ArrayList processes;
+    
+    public double getLaborTotal(){ return 0;}
+    public double getMaterialTotal(){return 0;}
+    
+    
+}

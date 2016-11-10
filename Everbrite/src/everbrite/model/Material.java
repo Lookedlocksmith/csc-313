7628d0bf860256e14fa90d10f3ec86cab2e3250a
Whitehurst <Whitehurst@I3-623-ITB>
2016-11-10 12:06:22 -0500

    Created Material class

---
+++ Everbrite/src/everbrite/model/Material.java
@@ -0,0 +1,15 @@
+package everbrite.model;
+
+/**
+ *
+ * @author Whitehurst
+ */
+public class Material {
+    
+    private String name;
+    private String type;
+    private String desc;
+    private double cost;
+    private boolean stocked;
+    
+}
